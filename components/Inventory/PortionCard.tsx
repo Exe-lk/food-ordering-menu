91d84be0b114a19cb1df760d616fc897f1de2773
Sasitha Ruvin <sasitharuween@gmail.com>
2025-01-28 16:58:42 +0530

    0.2.4
    UI Chanfes

--- components/Inventory/PortionCard.tsx
+++ components/Inventory/PortionCard.tsx
@@ -14,15 +14,15 @@ interface PortionCardProps {
 
 const PortionCard = ({ portions, onEdit, onRemove }: PortionCardProps) => {
   return (
-    <div className="mt-5 space-y-6"> {/* Increased gap between cards */}
+    <div className="mt-5 space-y-6"> 
       {portions.map((portion, index) => (
         <div 
           key={index} 
-          className="flex items-center justify-between border rounded-lg shadow-md p-4 bg-white hover:bg-gray-300 transition-all duration-300"
+          className="flex items-center justify-between border rounded-lg shadow-md p-4 bg-white hover:bg-gray-300 transition-all duration-300 cursor-pointer"
         >
           <div className="text-black font-semibold w-40 text-center">{portion.name}</div> {/* Centered text */}
           <div className="text-gray-700 w-40 text-center">{portion.serves}</div> {/* Fixed width & centered */}
-          <div className="flex space-x-2 justify-center w-40"> {/* Fixed width for uniformity */}
+          <div className="flex space-x-2 justify-center w-96"> {/* Fixed width for uniformity */}
             <button
               onClick={() => onEdit(index)}
               className="px-4 py-2 w-24 text-white bg-customblue hover:bg-blue-900 rounded-md cursor-pointer"
